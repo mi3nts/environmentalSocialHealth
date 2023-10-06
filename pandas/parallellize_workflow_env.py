7a19138ff8713582800b3fbc9234a73ee7a8adf9
Ashen <aaf170130@utdallas.edu>
2023-10-06 13:22:25 -0500

    functionality to create models over all icd and parallelize on s76

--- pandas/parallellize_workflow_env.py
+++ pandas/parallellize_workflow_env.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import os 
+import xarray as xr
+import geopandas as gpd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 import xgboost as xgb
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -9,13 +16,12 @@ from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, E
 from sklearn.svm import SVR
 import numpy as np
 import seaborn as sns
-from statsmodels.graphics.gofplots import qqplot_2samples
-import os 
-import xarray as xr
-import geopandas as gpd
-import matplotlib.pyplot as plt
+import multiprocessing
+import time
 import shap
 import re
+from statsmodels.graphics.gofplots import qqplot_2samples
+
 
 root = "../CAMS/"
 units = "../assets/CAMS_units.csv"
@@ -36,6 +42,7 @@ icd10_subset = hospital_quarters[hospital_quarters.index('2015q4'):hospital_quar
 
 def numerical_sort(filename):
     return [int(x) if x.isdigit() else x for x in re.split(r'(\d+)', filename)]
+    
 
 def grabWeatherData(time_period):
     means = pd.DataFrame()
@@ -54,7 +61,7 @@ def grabWeatherData(time_period):
         for i in sorted_csvs:
             df = pd.read_csv(i, skiprows=0, usecols=lambda x: x != 'Unnamed: 0')
             means = pd.concat([means, df])
-        
+
         # print(means.head())
 
         # means.insert(0, 'PAT_ZIP', tx_zip['ZCTA5CE10'].values)
@@ -89,21 +96,18 @@ nice_names['pm2p5'] = 'Particulate matter 2.5um'
 env_data = env_data.copy().dropna(axis=1, how='all')
 env_data = env_data.dropna()
 
-print("\n\n Environmental data processed. \n\n")
-
-quarter_label = dict(zip(env_quarters, [i.upper() for i in icd9_subset + icd10_subset]))
-
+print("Data loading done")
 
 def eval_results(y_test, predictions, y_train, train_preds):
     train_acc, test_acc = [], []
     mse = mean_squared_error(y_test, predictions)
-    # print("RMSE: ", np.sqrt(mse))
+    print("RMSE: ", np.sqrt(mse))
     train_r2 = r2_score(y_train, train_preds)
     test_r2 = r2_score(y_test, predictions)
     train_acc.append(train_r2)
     test_acc.append(test_r2)
-    # print("Train R2: ", train_r2)
-    # print("Test R2: ", test_r2)
+    print("Train R2: ", train_r2)
+    print("Test R2: ", test_r2)
     #print(train_preds)
 
     # bin_lbl = bin_labels['labels'].loc[bin_labels['bin_id'] == item].values[0]
@@ -114,6 +118,26 @@ def eval_results(y_test, predictions, y_train, train_preds):
     #print('len of full pdf inside fxn', len(full_pdf))
     
     return mse, train_r2, test_r2, train_acc, test_acc, full_pdf
+
+def test_train_plot(full_pdf, y_test, train_r2, y_train, X_train, test_r2, X_test, title, save_path):
+    plt.figure(figsize=(12, 8)) 
+    # print('inside r2 plot')
+    #print('len of full pdf inside plot fxn', len(full_pdf)) 
+    
+    g = sns.jointplot(data=full_pdf, x='Predicted', y='Actual', hue='Legend', alpha=0.3, #cmap="Blues", 
+                          marginal_kws=dict(bw_adjust=0.2, cut=0))
+    g.set_axis_labels('Predicted ($\log_{10}$(# ICD Codes/Zip Code Population))', 'Actual ($\log_{10}$(# ICD Codes/Zip Code Population))')
+    set_min_plot = y_train
+    g.ax_joint.plot([min(set_min_plot), max(set_min_plot)], [min(set_min_plot), max(set_min_plot)], color='k',label="1:1")
+    handles, labels = g.ax_joint.get_legend_handles_labels()
+    g.ax_joint.legend(handles=handles, labels=[ f'Train: {train_r2:.2f}, N = {len(X_train):,}', f'Test: {test_r2:.2f}, N={len(X_test):,}','1:1',], title=None)
+    g.fig.suptitle(title, size=20)
+
+    plt.tight_layout()
+    plt.subplots_adjust(hspace=0, wspace=0)
+    plt.savefig(save_path, bbox_inches='tight')
+    plt.clf()
+    # plt.show()
 
 def XGB_model(X_train, X_test, y_train, y_test):
     # dt = xgb.DMatrix(X_train, label=y_train.values)
@@ -134,7 +158,6 @@ def XGB_model(X_train, X_test, y_train, y_test):
         # "base_score": np.mean(y_test),
         "eval_metric": "rmse"
     }
-    # model = xgb.train(params, dt, 300, [(dt, "train"),(dv, "valid")])#, early_stopping_rounds=10)
     model = xgb.train(
     params=params, 
     dtrain=dt, 
@@ -142,48 +165,14 @@ def XGB_model(X_train, X_test, y_train, y_test):
     evals=[(dt, "train"), (dv, "valid")],
     early_stopping_rounds=10,
     verbose_eval=False  # You can set verbose_eval to True or False as needed
-)
-
-    
+    )
     predictions = model.predict(dv)
     train_preds = model.predict(dt)
     
     return model, predictions, train_preds
 
-def test_train_plot(full_pdf, y_test, train_r2, y_train, X_train, test_r2, X_test, title, save_path):
-    plt.figure(figsize=(12, 8)) 
-    print('inside r2 plot')
-    #print('len of full pdf inside plot fxn', len(full_pdf)) 
-    
-    g = sns.jointplot(data=full_pdf, x='Predicted', y='Actual', hue='Legend', alpha=0.3, #cmap="Blues", 
-                          marginal_kws=dict(bw_adjust=0.2, cut=0))
-    g.set_axis_labels('Predicted ($\log_{10}$(# ICD Codes/Zip Code Population))', 'Actual ($\log_{10}$(# ICD Codes/Zip Code Population))')
-    set_min_plot = y_train
-    g.ax_joint.plot([min(set_min_plot), max(set_min_plot)], [min(set_min_plot), max(set_min_plot)], color='k',label="1:1")
-    handles, labels = g.ax_joint.get_legend_handles_labels()
-    g.ax_joint.legend(handles=handles, labels=[ f'Train: {train_r2:.2f}, N = {len(X_train):,}', f'Test: {test_r2:.2f}, N={len(X_test):,}','1:1',], title=None)
-    g.fig.suptitle(title, size=20)
-
-    plt.tight_layout()
-    plt.subplots_adjust(hspace=0, wspace=0)
-    plt.savefig(save_path, bbox_inches='tight')
-    plt.clf()
-    # plt.show()
-
-    
-def plot_qq(full_pdf, save_path):
-    print('inside qq plot')
-    fig = qqplot_2samples(full_pdf['Predicted'],full_pdf['Actual'],line='45', 
-                    xlabel='Predicted ($\log_{10}$(# ICD Codes/Zip Code Population))',  
-                    ylabel='Actual ($\log_{10}$(# ICD Codes/Zip Code Population))')
-    plt.title(f'Quantile-Quantile plot')
-
-    # plt.show()
-    plt.savefig(save_path, bbox_inches='tight')
-    # plt.show()
-
 def shap_plots(model, X_test, save_path, title):
-    print('inside shap plot')
+    # print('inside shap plot')
     explainer = shap.TreeExplainer(model)
 
     # feature_names = [nice_names[i] for i in X_test.columns]
@@ -213,14 +202,45 @@ def shap_plots(model, X_test, save_path, title):
     plt.title(f'{title}') 
     plt.savefig(save_path, bbox_inches='tight')
     # plt.show()
+    
+def plot_qq(full_pdf, save_path):
+    # print('inside qq plot')
+    fig = qqplot_2samples(full_pdf['Predicted'],full_pdf['Actual'],line='45', 
+                    xlabel='Predicted ($\log_{10}$(# ICD Codes/Zip Code Population))',  
+                    ylabel='Actual ($\log_{10}$(# ICD Codes/Zip Code Population))')
+    plt.title(f'Quantile-Quantile plot')
 
-icd_codes = ['I5042', 'A4189', 'I509', 'I2510','F209','G309', 'J4521']
-icd_codes = os.listdir('../icd10/')
+    # plt.show()
+    plt.savefig(save_path, bbox_inches='tight')
+    # plt.show()
 
-def getDF(icd_codes):
+# def shap_plots(model, X_test):
+#     explainer = shap.TreeExplainer(model)
+#     # X_test = pd.DataFrame(X_test)
+#     # X_test.columns = [i if i not in corr_labels.keys() else corr_labels[i] for i in features]
+#     # print(X_test.head)
+#     shap_values = explainer.shap_values(X_test)
+
+#     shap.summary_plot(shap_values, X_test, plot_type='dot', max_display=10, show=False)
+#     # print('part2')
+#     fig, ax = plt.gcf(), plt.gca()
+#     feature_order = np.argsort(np.sum(np.abs(shap_values), axis=0))
+#     # print(feature_order)
+#     # ax.set_yticklabels([corr_labels[i] for i in selected_columns])
+#     # ax.set_yticklabels([selected_columns[i] for i in feature_order])
+
+#     # ax.set_yticklabels([corr_labels[i] for i in features.columns[feature_order]])
+#     plt.xlabel('Average Impact on Model')
+#     # plt.title(f'Feature Importance Ranking for {plot_title}{pm_labels[item]}')
+#     # print(f"Shap summary for {item}")
+#     plt.title(f'SHAP Values') 
+#     plt.show()
+nthresh = 3
+os.makedirs(f"./Results_nthresh_{nthresh}", exist_ok=True)
+
+def getDF(icd_codes): # this is the parallel function
     fits_data = []
-    nthresh = 3
-    data_list = []
+
     for icd_code in icd_codes:
         df = pd.DataFrame()
         for quarter in hospital_quarters[:-1]: # [:-1] to avoid 2022q1 
@@ -240,10 +260,9 @@ def getDF(icd_codes):
             # full_df = census_df.merge(env_icd, on='PAT_ZIP')
             full_df = env_df.merge(icd_df, on='PAT_ZIP')
             full_df['pop_density'] = full_df['population']/(full_df['LandArea_sqm']/1_000_000)
-        
 
             df = pd.concat([df, full_df])
-        
+
         os.makedirs(f'./Plots/{icd_code}', exist_ok=True)
 
         data = df.copy()
@@ -252,14 +271,15 @@ def getDF(icd_codes):
         # print(len(data_quality))
         data_quality = data_quality[data_quality['ICD'] >= nthresh]
         #print(f'Len of data at no threshold: {len(data)}, len of data at threshold: {len(data_quality)}')
-        
+
         # data_quality = data_quality.drop([data_quality.columns[data_quality.isna().any()]][0], axis=1)
         # print(len(data_quality))
         # data_quality = np.log10(data_quality)
-        
+
         data_quality.replace([np.inf, -np.inf], np.nan, inplace=True)
         data_quality = data_quality.dropna(axis=0)
-        data_list.append(data_quality)
+        if len(data_quality) < 2000:
+            continue
         # print(data_quality.columns)
         # print(len(data_quality))
         # data_quality = data_quality[data_quality['median household income'] > 0]
@@ -269,17 +289,16 @@ def getDF(icd_codes):
 
         # "chnk", \
         X = data_quality.loc[:,["d2m","t2m", "lai_hv","lai_lv", 
-        "pm10","pm2p5","stl1",
-        #"sp",
-        "co", # "aermr04","aermr05","aermr06", 
-        "c2h6","hcho","aermr09","aermr07","aermr10",
-        "aermr08","oh", #"c5h8", #"ch4_c",
-        "hno3","no2","no","go3","pan",
-        "c3h8", #"aermr01","aermr02","aermr03",
-        "aermr12",
-        # "aermr11",
-        "so2"]] #, "pop_density"]] # including only relevant environmental data
-        # "median household income", \
+            "pm10","pm2p5","stl1",
+            #"sp",
+            "co", # "aermr04","aermr05","aermr06", 
+            "c2h6","hcho","aermr09","aermr07","aermr10",
+            "aermr08","oh", #"c5h8", #"ch4_c",
+            "hno3","no2","no","go3","pan",
+            "c3h8", #"aermr01","aermr02","aermr03",
+            "aermr12",
+            # "aermr11",
+            "so2"]]# "median household income", \
         # "hispanic",
         # # "aggregate income",\
         # "males college < 1yr", \
@@ -308,15 +327,15 @@ def getDF(icd_codes):
         y_train_scaled = target_scaler.fit_transform(y_train.to_numpy().reshape(-1,1))
         y_test_scaled = target_scaler.transform(y_test.to_numpy().reshape(-1,1))
 
-        
-        # model = GradientBoostingRegressor()
+        # model = RandomForestRegressor()
+        # # model = GradientBoostingRegressor()
 
-        # model = ExtraTreesRegressor()
+        # # model = ExtraTreesRegressor()
 
-        # Train and predict using each model
-        
-        # for model_name, model in models.items():
-        # print('X_train', len(X_train_scaled), ' y_test', len(y_test))
+        # # Train and predict using each model
+        # predictions = {}
+        # # for model_name, model in models.items():
+        # # print('X_train', len(X_train_scaled), ' y_test', len(y_test))
         # model.fit(X_train_scaled, y_train)
         # y_pred = model.predict(X_test_scaled)
 
@@ -328,11 +347,6 @@ def getDF(icd_codes):
 
         # Get feature importances
 
-        # things that worked
-
-        # model = RandomForestRegressor()
-        # predictions = {}
-        # model.fit(X_train_scaled, y_train)
         icd_code_title = icd_code
         # y_pred = model.predict(X_test_scaled)
         # r2_scores = r2_score(y_test, y_pred)
@@ -342,23 +356,20 @@ def getDF(icd_codes):
                                                y_train, y_test)
 
 
+
         mse, train_r2, test_r2, train_acc, test_acc, full_pdf = eval_results(y_test, y_pred, y_train, train_preds)
         # print('full pdf', len(full_pdf))
-        print('sus error start')
-    
+        # print('full pdf', len(full_pdf))
         test_train_plot(full_pdf, y_test, train_r2, y_train, X_train_scaled, test_r2, X_test_scaled,
-                        title=f"ICD-10 Codes for {icd_code_labels[icd_code_title]} \n # threshold = {nthresh}, Environmental data \n from {start_year} to {end_year-1}",
+                        title=f"ICD-10 Codes for {icd_code_title} \n # threshold = {nthresh}, Environmental data \n from {start_year} to {end_year-1}",
                         save_path=f'./Plots/{icd_code}/{icd_code}_r2.png'
                         )
-        print('sus error end')
-        
-        
-        # don't use with XGBoost
 
+        
+        
         # importance_scores = model.feature_importances_
         # feature_names = [nice_names[i] for i in X.columns]
         # # feature_names = X.columns
-        # # feature_names = [(i, nice_names[i]) for i in X.columns]
 
         # # Sort feature importances in descending order
         # indices = importance_scores.argsort()[::-1][:20]
@@ -372,17 +383,19 @@ def getDF(icd_codes):
 
 
 
-        # plt.title(f'Feature Importance Ranking for Environmental data model on {icd_code_labels[icd_code_title]}')
+        # plt.title(f'Feature Importance Ranking for Environmental data model on {icd_code_title}')
         # plt.ylabel('Features')
         # plt.xlabel('Feature Importance Ranking')
-        # # plt.savefig(f'./Plots/{icd_code}/{icd_code}_feat_imp.png')
-        # plt.show()
-        # print(rmse_list)
+        # # plt.savefig(f'./Plots/{icd_code}/{icd_code}_feat_imp.png', bbox_inches='tight')
+        # #plt.show()
+        # # print(rmse_list)
 
+        # shap_plots(model, X_test)
         shap_plots(model, 
             X_test, 
             f'./Plots/{icd_code}/{icd_code}_shap.png', 
-            title=f"SHAP Values for {icd_code_labels[icd_code_title]}")
+            title=f"SHAP Values for {icd_code_title}")
+
 
         plot_qq(full_pdf, f'./Plots/{icd_code}/{icd_code}_qq.png')
 
@@ -390,32 +403,37 @@ def getDF(icd_codes):
 
         fits_data.append([icd_code, train_r2, test_r2, np.sqrt(mse), len(X)])
 
-    fits_df = pd.DataFrame(fits_data)
-    fits_df.columns = ['ICD', 'train_r2', 'test_r2', 'rmse', 'numDataPoints']
+    return fits_data
 
-    return data_list
+    # fits_df = pd.DataFrame(fits_data)
+    # fits_df.columns = ['ICD', 'train_r2', 'test_r2', 'rmse', 'numDataPoints']
+
+    # fits_df.to_csv(f'th={nthresh} - results.csv')
+    
+
+num_workers = multiprocessing.cpu_count()
+icd_codes = [i for i in os.listdir(icd_data)]
+# icd_codes = icd_codes[:54]
+    
+for i in range(42): # change these hard coded numbers 
+
+        pool = multiprocessing.Pool(processes=num_workers)
+        
+        start_time = time.time()
+        zip_olist = pool.map(getDF, [[icd_code] for icd_code in icd_codes[(i)*num_workers:(i+1)*num_workers]])
+        end_time = time.time()
+
+        print(f'Took {end_time - start_time} seconds for execution for set #{i}')
+
+        pool.close()
+        pool.join()
+             
+        # print(zip_olist)
+        result = pd.DataFrame([i[0] for i in zip_olist if len(i) > 0])
+        # print(result)
+        result.columns = ['ICD', 'train_r2', 'test_r2', 'rmse', 'numDataPoints']
+        
+        result.to_csv(f"./Results_nthresh_{nthresh}/multiprocess_df_{i}.csv")
 
 
-    # fits_df.to_csv(f'n_thresh = {nthresh} - results.csv')
 
-icd_code_labels = {
-    'A4189': 'A41.89 - Other Specified Sepsis',
-    'B9789': 'B97.89 - Other viral agents as the cause \n of diseases classified elsewhere',
-    'J157' : 'J15.7 - Pneumonia due to Mycoplasma pneumoniae',
-    'J449' : 'J44.9 - Chronic obstructive pulmonary disease, unspecified',
-    'J45998' : 'J45.998 - Other asthma',
-    'J4521' : 'J45.21 - Mild intermittent asthma \n with (acute) exacerbation', 
-    'F068' : 'F06.8 - Other specified mental disorders \n due to known physiological condition', 
-    'I2510' : 'I25.10 - Atherosclerotic heart disease \n of native coronary artery without angina pectoris',
-    'I509' : 'I50.9 - Heart failure, unspecified', 
-    'A4189' : 'A41.89 - Other specified sepsis',
-    'I5041' : 'I50.41 - Acute combined systolic (congestive) \n and diastolic (congestive) heart failure',
-    'I5042' : 'I50.42 - Chronic combined systolic (congestive) \n diastolic (congestive) heart failure',
-    'F209' : 'Schizophrenia, unspecified'
-}
-
-icd_codes = ['A4189', 'I5042','I509', 'I2510','F209','G309', 'J4521']
-icd_codes_subset = list(icd_code_labels.keys())[:1]
-
-print("Creating Models... \n\n")
-data_for_icd = getDF(icd_codes_subset)
